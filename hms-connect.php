80e1af1d7f16ab6c145bc00fd0ce76539588c00d
Mike England <micheallengland@gmail.com>
2018-09-13 11:52:37 -0400

    Update hms-connect.php

--- hms-connect.php
+++ hms-connect.php
@@ -13,28 +13,13 @@ Domain Path: /languages
 
 
 <?php
-// Updater Class
-class Smashing_Updater {
-  protected $file;
-  protected $plugin;
-  protected $basename;
-  protected $active;
+// Include our updater file
+include_once( plugin_dir_path( __FILE__ ) . 'update.php');
 
-  public function __construct( $file ) {
-    $this->file = $file;
-    /**
-     * 'admin_init' Ensures that we have the function to get plugin data & that the plugin is active.
-     */ 
-    add_action( 'admin_init', array( $this, 'set_plugin_properties' ) );
-    return $this;
-  }
-
-  public function set_plugin_properties() {
-    $this->plugin   = get_plugin_data( $this->file );
-    $this->basename = plugin_basename( $this->file );
-    $this->active   = is_plugin_active( $this->basename );
-  }
-}
+$updater = new Smashing_Updater( __FILE__ ); // instantiate our class
+$updater->set_username( 'michealengland' ); // set username
+$updater->set_repository( 'hms-connect' ); // set repo
+$updater->initialize(); // initialize the updater
 
 function hms_connect_run() {
     echo '<h1>test</h1>';

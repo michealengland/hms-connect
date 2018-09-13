2412874fb29f8002cd9a4a29382b3136398dbd68
Mike England <micheallengland@gmail.com>
2018-09-13 11:29:16 -0400

    Update hms-connect.php

--- hms-connect.php
+++ hms-connect.php
@@ -1,1 +1,43 @@
+<?php
+/*
+Plugin Name: HMS Connect
+Plugin URI: https://manage.thinkholmes.com/hms-connect
+Description: Easily deploy add ons that enhance functionality. 
+Version: 0.0.1
+Author: Micheal England
+Author URI: http://michealengland.com
+Text Domain: hms-connect
+Domain Path: /languages
+*/
+?>
 
+
+<?php
+// Updater Class
+class Smashing_Updater {
+  protected $file;
+  protected $plugin;
+  protected $basename;
+  protected $active;
+
+  public function __construct( $file ) {
+    $this->file = $file;
+    /**
+     * 'admin_init' Ensures that we have the function to get plugin data & that the plugin is active.
+     */ 
+    add_action( 'admin_init', array( $this, 'set_plugin_properties' ) );
+    return $this;
+  }
+
+  public function set_plugin_properties() {
+    $this->plugin   = get_plugin_data( $this->file );
+    $this->basename = plugin_basename( $this->file );
+    $this->active   = is_plugin_active( $this->basename );
+  }
+}
+
+function hms_connect_run() {
+    echo '<h1>test</h1>';
+}
+
+hms_connect_run();
